bfdd80da240b0b894f3203d3c68774e724f221fa
gyks <gyksqop@gmail.com>
2020-03-14 16:22:38 +0100

    adding function to resend picture with the same artist

--- handler/commands.js
+++ handler/commands.js
@@ -1,27 +1,175 @@
-const request = require("request");
-const baseUrl = "https://api.vk.com/method/";
 require("dotenv").config();
-const TOKEN = process.env.TOKEN;
-const v = "5.103";
+const vkMethods = require("./vkApiMethods");
+const fgo = require("./fgoGacha");
+const api_key = process.env.API_TOKEN_DANBOORU;
+const login = process.env.DANBOORU_LOGIN;
+const HTMLParser = require("node-html-parser");
+const axios = require("axios");
 
-function sendMessage(peer_id, message) {
-  let url = baseUrl + "messages.send";
-  request(
-    {
-      url: url,
-      method: "POST",
-      form: {
-        peer_id: peer_id,
-        message: message,
-        random_id: Math.random(),
-        access_token: TOKEN,
-        v: v
+const commandsList = {
+  найди: findPicture,
+  гача: simulateGacha,
+  техас: sendTexas
+};
+
+async function findPicture(msgInfoObject, picUrl) {
+  // определяем есть ли picUrl, если нет смотрим в аттачах
+  const addHttps = m => {
+    m.substring(0, 4) == "http" ? m : (m = "https:" + m);
+    return m;
+  };
+
+  if (picUrl && picUrl.includes("https://")) {
+  } else if (msgInfoObject.attachments.length) {
+    picUrl = msgInfoObject.attachments[0].photo.sizes.slice(-1)[0].url;
+  } else if (msgInfoObject.reply_message.attachments.length) {
+    picUrl = msgInfoObject.reply_message.attachments[0].photo.sizes.slice(-1)[0]
+      .url;
+  } else {
+    vkMethods.sendMessage(msgInfoObject.peer_id, "Неправилный запрос.");
+    return;
+  }
+
+  const body = await axios.get("https://iqdb.org/", {
+    params: {
+      url: picUrl
+    }
+  });
+
+  let parsed = HTMLParser.parse(body.data).querySelectorAll(
+    "#pages.pages div table tr"
+  );
+  // На iqdb первые 4 вхождения по правилу выше - картинка, которую мы отправили, убираем их
+  parsed = parsed.slice(4);
+
+  // Группируем инфу о каждом совпадении по 5 (кол-во tr в table для каждой пикчи)
+
+  let pictureInfo = []; //массив инфы по одной картинке
+  let pictureLinks = []; //массив линков на посты с картинками для поиска дополнительной картинки по автору
+  let message = "Я нашла это, десу: \n";
+  for (let i = 0; i < parsed.length; i++) {
+    pictureInfo.push(parsed[i]);
+    // группируем по 5 и пихаем нужную инфу в сообщение, очищаем инфу для следующей пикчи
+    if ((i + 1) % 5 == 0) {
+      // всратый DOM требует всратый парсер...
+      let picLink = addHttps(
+        pictureInfo[1]
+          .querySelectorAll(".image a")[0]
+          .rawAttrs.slice(6)
+          .slice(0, -1)
+      );
+
+      pictureLinks.push(picLink);
+      message +=
+        picLink +
+        " с " +
+        pictureInfo[4]
+          .querySelectorAll("td")[0]
+          .childNodes[0].toString()
+          .slice(0, 4) +
+        "похожестью" +
+        "\n";
+      pictureInfo = [];
+    }
+  }
+
+  // функция, которая принимает ссылку на пост на данбуре и выдаёт урлу картинки того же автора
+  const findArtistSendAnotherPic = async picLink => {
+    const toLjson = l => {
+      // если нет danbooru, то ничего не присылаем
+      if (l.includes("danbooru") ? false : true) return null;
+      return l + ".json";
+    };
+    picLink = toLjson(picLink);
+    if (picLink === null) return;
+    const body = await axios.get(picLink, {
+      params: {
+        login: login,
+        api_key: api_key
       }
-    },
-    function(error, response, body) {
-      //console.log(response.toJSON());
+    });
+    const artist = body.data.tag_string_artist;
+    const anotherPic = await axios.get(
+      "https://danbooru.donmai.us/posts.json",
+      {
+        params: {
+          login: login,
+          api_key: api_key,
+          tags: artist,
+          limit: 1,
+          random: true
+        }
+      }
+    );
+    return anotherPic.data[0].large_file_url;
+  };
+
+  // если команда расширенная, то ищем ещё одну картинку с этим автором и кидаем
+  if (msgInfoObject.text.includes("еще")) {
+    for (let picLink of pictureLinks) {
+      const anotherPicUrl = await findArtistSendAnotherPic(picLink);
+      if (anotherPicUrl) {
+        vkMethods.uploadPhotoViaUrlAsync(
+          msgInfoObject.peer_id,
+          anotherPicUrl,
+          photoInfo => {
+            vkMethods.sendMessage(
+              msgInfoObject.peer_id,
+              "Я ещё кое-что нашла!",
+              "photo" +
+                photoInfo.owner_id +
+                "_" +
+                photoInfo.id +
+                "_" +
+                photoInfo.access_key
+            );
+          }
+        );
+        break;
+      }
+    }
+  } else {
+    // если команда не расширена, то выдаём обычный результат
+    vkMethods.sendMessage(msgInfoObject.peer_id, message);
+  }
+}
+
+function simulateGacha(msgInfoObject) {
+  let pulled = fgo.gacha();
+  let msg = "Вы выбили это, десу: \n";
+  for (let item of pulled) {
+    msg += item + "; \n";
+  }
+  vkMethods.sendMessage(msgInfoObject.peer_id, msg);
+}
+
+async function sendTexas(msgInfoObject) {
+  const body = await axios.get("https://danbooru.donmai.us/posts.json", {
+    params: {
+      login: login,
+      api_key: api_key,
+      tags: "texas_(arknights)",
+      limit: 1,
+      random: true
+    }
+  });
+  texasPictureUrl = body.data[0].large_file_url;
+  vkMethods.uploadPhotoViaUrlAsync(
+    msgInfoObject.peer_id,
+    texasPictureUrl,
+    photoInfo => {
+      vkMethods.sendMessage(
+        msgInfoObject.peer_id,
+        "Ваша тексас!",
+        "photo" +
+          photoInfo.owner_id +
+          "_" +
+          photoInfo.id +
+          "_" +
+          photoInfo.access_key
+      );
     }
   );
 }
 
-exports.sendMessage = sendMessage;
+module.exports = commandsList;
--- handler/handler.js
+++ handler/handler.js
@@ -1,64 +1,10 @@
-const sendMessage = require("./commands");
-const fgo = require("./fgoGacha");
-const util = require("util");
-const request = require("request");
-
-const commandsList = {
-  найди: findPicture,
-  гача: simulateGacha
-};
-
-function findPicture(msgInfoObject, picUrl) {
-  url = "http://iqdb.org/?url=";
-
-  if (picUrl) {
-    url += picUrl;
-  } else if (msgInfoObject.attachments.length) {
-    url += msgInfoObject.attachments[0].photo.sizes.slice(-1)[0].url;
-  } else if (msgInfoObject.reply_message.attachments.length) {
-    url += msgInfoObject.reply_message.attachments[0].photo.sizes.slice(-1)[0]
-      .url;
-  } else {
-    sendMessage.sendMessage(msgInfoObject.peer_id, "Неправилный запрос.");
-    return;
-  }
-  request(
-    {
-      url: url,
-      method: "GET"
-    },
-    function(error, response, body) {
-      let regex = /<a href="(.*?)">/gm;
-      var count = 0;
-      let msg = "Я нашла это: ";
-      for (let item of body.matchAll(regex)) {
-        if (count++ == 2) break;
-        msg += " https:" + item[1];
-      }
-      sendMessage.sendMessage(msgInfoObject.peer_id, msg);
-    }
-  );
-}
-
-function simulateGacha(msgInfoObject) {
-  let pulled = fgo.gacha();
-  let msg = "Вы выбили это, десу: \n";
-  for (let item of pulled) {
-    msg += item + "; \n";
-  }
-  sendMessage.sendMessage(msgInfoObject.peer_id, msg);
-}
+const commandsList = require("./commands");
 
 exports.handle = vkEventRequest => {
   if (vkEventRequest.type == "message_new") {
     const msgInfoObject = vkEventRequest.object.message;
     const text = msgInfoObject.text;
-    /*if (msgInfoObject.from_id == "-163683277") {
-      sendMessage.sendMessage(
-        msgInfoObject.peer_id,
-        "Да что ты говоришь, Костян!"
-      );
-    }*/
+
     if (text.toLowerCase().includes("мейда")) {
       // Проверяем, что в сообщение есть ключевое обращение
       let text_arr = text.split(" ");
@@ -67,27 +13,7 @@ exports.handle = vkEventRequest => {
       // то выполняем её с аргументами на следующих позициях(если они есть)
       if (text_arr[1] in commandsList) {
         commandsList[text_arr[1]](msgInfoObject, text_arr[2]);
-        //Дебажное сообщение юзеру о начале работы.
-
-        /*sendMessage.sendMessage(
-          msgInfoObject.peer_id,
-          `Приступаю к выполнению команды "${text_arr[1]}" Аргументы: ${
-            text_arr[2] ? text_arr[2] : "отсутствуют."
-          }`
-        );*/
       }
-
-      /* console.log(
-        util.inspect(
-          msgInfoObject.reply_message,
-          false,
-          null,
-          true 
-        )
-      );
-      console.log(
-        msgInfoObject.reply_message.attachments[0].photo.sizes.slice(-1)[0].url
-      );*/
     }
   }
 };
